79ac157a0bef29b1569efa18b4efdcddf4fdc8c3
Seter2001 <74210539+Seter2001@users.noreply.github.com>
2020-12-13 17:55:37 +0100

    Added bash folder

---
+++ bash/szpieg/szpieg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "Sprawdzenie aktywnosci"
+
+finger seter > zbior.txt
+
+if [ "$(cat zbior.txt | grep "On since" )" != "" ]
+then
+    
+    echo "Seter aktywny"
+    
+else
+    
+    echo "Seter nie aktywny"
+    
+fi
